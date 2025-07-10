ec2d9bec0e61a91d2e5088bddff3355870f833fe
Natanael <taelima1997@gmail.com>
2025-07-10 17:11:47 -0300

    Add functions for accessing invite links and subscribing to events with referral tracking

---
+++ src/functions/subscribe-to-event.ts
@@ -0,0 +1,42 @@
+import { eq } from 'drizzle-orm'
+import { db } from '../drizzle/client'
+import { schema } from '../drizzle/schema'
+import { redis } from '../redis/client'
+import { EmailAlreadyExists } from './errors/email-already-exists'
+
+interface SubscribeToEventParams {
+  name: string
+  email: string
+  invitedBySubscriberId: string | null
+}
+
+export async function subscribeToEvent({
+  name,
+  email,
+  invitedBySubscriberId,
+}: SubscribeToEventParams) {
+  const results = await db
+    .select()
+    .from(schema.subscriptions)
+    .where(eq(schema.subscriptions.email, email))
+
+  if (results.length > 0) {
+    throw new EmailAlreadyExists()
+  }
+
+  const [{ subscriberId }] = await db
+    .insert(schema.subscriptions)
+    .values({
+      name,
+      email,
+    })
+    .returning({
+      subscriberId: schema.subscriptions.id,
+    })
+
+  if (invitedBySubscriberId) {
+    await redis.zincrby('referral:ranking', 1, invitedBySubscriberId)
+  }
+
+  return { subscriberId }
+}
